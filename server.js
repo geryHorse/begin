dfbcbad8dd1f6bae43db67d0927ff457529a4032
mahui <mahui00000@163.com>
2017-05-20 23:34:23 +0800

    favicon.ico

--- server.js
+++ server.js
@@ -7,12 +7,8 @@ const url = require('url');
 function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
-        if (pathname !== '/favicon.ico') {
-            console.log('Request for ' + pathname + ' reveived');
-            route(handle, pathname, response);
-        } else {
-            response.end();
-        }        
+        console.log('Request for ' + pathname + ' reveived');
+        route(handle, pathname, response);
     }
     http.createServer(onRequest).listen(8888);
     console.log('Server has started');
