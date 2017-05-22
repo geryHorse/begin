881b353f0f218823676945c8e347d4cbfc720a40
mahui <mahui00000@163.com>
2017-05-22 21:51:46 +0800

    修改

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,7 +1,8 @@
 
 
 var querystring = require('querystring'),
-    fs = require('fs');
+    fs = require('fs')
+    formidable = require('formidable');
 
 function start(response) {
     console.log('Request handler "start" was called');    
@@ -24,17 +25,26 @@ function start(response) {
     response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
     console.log('Request handler "upload" was called');
 
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('You have send: ' + querystring.parse(postData).text);
-    response.end();
+    var form = new formidable.IncomingForm();
+    form.uploadDir = "tmp";
+    console.log('about to parse');
+    form.parse(request, function(error, fields, files) {
+        console.log('parsing done');
+        var name = files.upload.name;
+        fs.renameSync(files.upload.path, './tmp/' + name);
+        response.writeHead(200, {'Content-Type': 'text/html'});
+        response.write("received image:<br/>");
+        response.write("<img src='/show' />");
+        response.end();
+    });
 }
 
-function show(response, postData) {
+function show(response) {
     console.log('Request handler "show" was called');
-    fs.readFile('./tmp/a.jpg', 'binary', function(error, file) {
+    fs.readFile('./tmp/*.jpg', 'binary', function(error, file) {
         if (error) {
             response.writeHead(500, {'Content-Type': 'text/plain'});
             response.write(error + '\n');
--- server.js
+++ server.js
@@ -8,20 +8,7 @@ function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
         console.log('Request for ' + pathname + ' reveived');
-
-        var postData = '';
-
-        // request.setEncoding('utf8');
-
-        request.addListener('data', function(postDataChunk) {
-            postData += postDataChunk;
-            console.log('Received POST data chunk: ' + postDataChunk);
-            console.log('--------------------------------------')
-        });
-
-        request.addListener('end', function() {
-            route(handle, pathname, response, postData);            
-        });
+        route(handle, pathname, response, request);
     }
     http.createServer(onRequest).listen(8888);
     console.log('Server has started');
